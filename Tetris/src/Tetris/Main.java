af2777f8f5c86cfaca573bdc86140245835fb5b5
Juan Duarte <jdduarter@correo.udistrital.edu.co>
2020-08-26 10:21:24 -0500

    Mejoras

--- Tetris/src/Tetris/Main.java
+++ Tetris/src/Tetris/Main.java
@@ -3,9 +3,6 @@ package Tetris;
 public class Main {
     public static void main(String[] args) {
         Inicio i = new Inicio();
-        /*Tetris game = new Tetris();
-        game.setLocationRelativeTo(null);
-        game.setVisible(true);*/
 
     }
 
